b8e3e228aced324d3e573dd52a0f1a5f897a9b3d
alecley <alecleipereira@gmail.com>
2024-12-25 21:13:33 -0300

    fiz a introducao quase completa

--- Introdução-JS/arquivos-js/introducao-js/ham-menu.js
+++ Introdução-JS/arquivos-js/introducao-js/ham-menu.js
@@ -8,3 +8,85 @@ function menu(){
         menu.style.display = 'none'
     }
 }
+
+ function topico_1() {
+    let topico_0 = window.document.querySelector('article#topico-0')
+
+    let topico_1 = window.document.querySelector('article#topico-1')
+
+    let topico_2 = window.document.querySelector('article#topico-2')
+
+    let topico_3 = window.document.querySelector('article#topico-3')
+
+   
+    if ( 
+        getComputedStyle(topico_2).display === 'block' &&
+        getComputedStyle(topico_3).display === 'block'
+        ) {
+
+        topico_2.style.display = 'none'
+        topico_3.style.display = 'none'
+    } else{
+        topico_0.style.display = 'block'
+        topico_1.style.display = 'block'
+        topico_2.style.display = 'block'
+        topico_3.style.display = 'block'
+       
+    }
+ }
+
+ function topico_2() {
+    let topico_0 = window.document.querySelector('article#topico-0')
+
+    let topico_1 = window.document.querySelector('article#topico-1')
+
+    let topico_2 = window.document.querySelector('article#topico-2')
+
+    let topico_3 = window.document.querySelector('article#topico-3')
+
+   
+    if ( 
+        (getComputedStyle(topico_0).display === 'block') &&
+        (getComputedStyle(topico_1).display === 'block') &&
+        (getComputedStyle(topico_3).display === 'block') ) {
+            //deixo invisivel os demais
+        topico_0.style.display = 'none'
+        topico_1.style.display = 'none'
+        topico_3.style.display = 'none'
+    } else{    
+        topico_0.style.display = 'block'
+        topico_1.style.display = 'block'
+        topico_2.style.display = 'block'
+        topico_3.style.display = 'block'
+       
+    }
+ }
+
+ function topico_3(){
+    let topico_0 = window.document.querySelector('article#topico-0')
+
+    let topico_1 = window.document.querySelector('article#topico-1')
+
+    let topico_2 = window.document.querySelector('article#topico-2')
+
+    let topico_3 = window.document.querySelector('article#topico-3')
+
+   
+    if ( 
+        (getComputedStyle(topico_0).display === 'block') &&
+        (getComputedStyle(topico_1).display === 'block') &&
+        (getComputedStyle(topico_2).display === 'block') ) {
+            //deixo invisivel os demais
+        topico_0.style.display = 'none'
+        topico_1.style.display = 'none'
+        topico_2.style.display = 'none'
+    } else{    
+        topico_0.style.display = 'block'
+        topico_1.style.display = 'block'
+        topico_2.style.display = 'block'
+        topico_3.style.display = 'block'
+       
+    }
+ }
+
+ 
